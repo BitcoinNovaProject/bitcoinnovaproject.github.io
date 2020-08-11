3a5221e5ed96dad6cad4ec805cf209aef6aea68e
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2020-08-11 20:45:02 +0200

    10-6-2019

---
+++ assets/vendors/node/config.js
@@ -0,0 +1,7 @@
+const apiInterface = 'http://api.bitcoinnova.org:8135/'
+const nodes = [
+  { name: 'pool.bitcoinnova.org', hostname: '85.214.158.156', port: 45223, region: 'Germany' },
+  { name: '37.59.108.203', hostname: '37.59.108.203',  port: 45223, region: 'France' },
+  { name: 'superblockchain.con-ip.com', hostname: 'superblockchain.con-ip.com', port: 45223, region: 'Spain' },
+  { name: '149.202.44.54', hostname: '149.202.44.54', port: 45223, region: 'France' }
+]
