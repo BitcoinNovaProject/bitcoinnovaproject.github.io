cf004415853c0a0111f60672348730e6b5315a00
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2020-08-11 21:08:59 +0200

    11-12-2019

--- assets/lang/lang.js
+++ assets/lang/lang.js
@@ -2,44 +2,44 @@ $(document).ready(function(){
     
     $("#spanish").on('click',function(event){
         (function(){
-          $("[data-translate]").jqTranslate('assets/lang/es',{forceLang: 'es'});
+          $("[data-translate]").jqTranslate('/assets/lang/es',{forceLang: 'es'});
         })();
     });
 
       
     $("#english").on('click',function(event){
           (function(){
-            $("[data-translate]").jqTranslate('assets/lang/en',{forceLang: 'en'});
+            $("[data-translate]").jqTranslate('/assets/lang/en',{forceLang: 'en'});
           })();
     });
 
     $("#french").on('click',function(event){
       (function(){
-        $("[data-translate]").jqTranslate('assets/lang/fr',{forceLang: 'fr'});
+        $("[data-translate]").jqTranslate('/assets/lang/fr',{forceLang: 'fr'});
       })();
     });
 
     $("#russian").on('click',function(event){
       (function(){
-        $("[data-translate]").jqTranslate('assets/lang/ru',{forceLang: 'ru'});
+        $("[data-translate]").jqTranslate('/assets/lang/ru',{forceLang: 'ru'});
       })();
     });
 
     $("#chinese").on('click',function(event){
       (function(){
-        $("[data-translate]").jqTranslate('assets/lang/cn',{forceLang: 'cn'});
+        $("[data-translate]").jqTranslate('/assets/lang/cn',{forceLang: 'cn'});
       })();
     });
 
     $("#korean").on('click',function(event){
       (function(){
-        $("[data-translate]").jqTranslate('assets/lang/kr',{forceLang: 'kr'});
+        $("[data-translate]").jqTranslate('/assets/lang/kr',{forceLang: 'kr'});
       })();
     });
 
     $("#japanese").on('click',function(event){
       (function(){
-        $("[data-translate]").jqTranslate('assets/lang/jp',{forceLang: 'jp'});
+        $("[data-translate]").jqTranslate('/assets/lang/jp',{forceLang: 'jp'});
       })();
     });
 
