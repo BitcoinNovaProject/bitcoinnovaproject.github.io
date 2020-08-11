06343135c6cb000c879050b4d48ff61bab7028a6
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2020-08-11 21:11:06 +0200

    15-01-2020

---
+++ assets/js/cookie.js
@@ -0,0 +1,15 @@
+lang.init({
+
+  cookie: {
+    // cookie name
+    name: 'langCookie',
+    //  expiration <a href="https://www.jqueryscript.net/time-clock/">date</a>
+    expiry: 365,
+    // path
+    path: '/'
+  },
+
+  // If true, cookies will override the "currentLang" option if the cookie is set.
+  allowCookieOverride: true
+
+});
