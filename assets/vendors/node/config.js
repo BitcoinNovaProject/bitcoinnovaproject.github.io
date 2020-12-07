7595b9225753920abef4f67f31568b682619a751
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2020-12-07 23:06:35 +0100

    06-09-2020

--- assets/vendors/node/config.js
+++ assets/vendors/node/config.js
@@ -1,8 +1,14 @@
 const apiInterface = 'http://api.bitcoinnova.org:8135/'
 const nodes = [
-  { name: 'pool.bitcoinnova.org', hostname: '85.214.158.156', port: 45223, region: 'Germany' },
-  { name: '37.59.108.203', hostname: '37.59.108.203',  port: 45223, region: 'France' },
-  { name: 'superblockchain.con-ip.com', hostname: 'superblockchain.con-ip.com', port: 45223, region: 'Spain' },
-  { name: 'ecologicalbtn.con-ip.com', hostname: 'ecologicalbtn.con-ip.com', port: 44223, region: 'Spain' },
-  { name: '149.202.44.54', hostname: '149.202.44.54', port: 45223, region: 'France' }
+  { name: 'Node 1 (mn1.bitcoinnova.org)', hostname: 'mn1.bitcoinnova.org', port: 45223, region: 'Germany' },
+  { name: 'Node 2 (mn2.bitcoinnova.org)', hostname: 'mn2.bitcoinnova.org', port: 45252, region: 'Spain' },
+  { name: 'Node 3 (superblockchain.con-ip.com)', hostname: 'superblockchain.con-ip.com', port: 45223, region: 'Spain' },
+  { name: 'Node 4 (mn4.bitcoinnova.org)', hostname: 'mn4.bitcoinnova.org', port: 45254, region: 'Spain' },
+  { name: 'Node 5 (mn5.bitcoinnova.org)', hostname: 'mn5.bitcoinnova.org', port: 45255, region: 'Spain' },
+  { name: 'Node 6 (ecologicalbtn.con-ip.com)', hostname: 'ecologicalbtn.con-ip.com',  port: 44223, region: 'Spain' },
+  { name: 'Node 7 (mn7.bitcoinnova.org)', hostname: 'mn7.bitcoinnova.org', port: 45225, region: 'United States' },
+  { name: 'Node 8 (mn8.bitcoinnova.org)', hostname: 'mn8.bitcoinnova.org', port: 45223, region: 'France' },
+  { name: 'Node 9 (mn9.bitcoinnova.org)', hostname: 'mn9.bitcoinnova.org', port: 45223, region: 'Spain' },
+  { name: 'Node 10 (mn10.bitcoinnova.org)', hostname: 'mn10.bitcoinnova.org', port: 45231, region: 'Spain' },
+  { name: 'Node 11 (mn11.bitcoinnova.org)', hostname: 'mn11.bitcoinnova.org', port: 45232, region: 'Spain' }
 ]
